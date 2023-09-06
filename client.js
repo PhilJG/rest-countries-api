a18cde76563f638f0561c19673ba7e946cda4f65
Phil J Gray <phl.j.gray@gmail.com>
2023-09-06 06:21:10 -0700

    Saved server.js and client.js

--- client.js
+++ client.js
@@ -1,3 +1,24 @@
 // Create a client-side file (e.g., client.js) that handles the client-side logic and interacts with the server-side endpoints.
 // Move the client-side code from the combined code you provided into the client.js file. This code will handle the client-side functionality, such as handling the dropdown selection and making requests to the server-side endpoints.
 // In the client-side code, update the event listener to make an HTTP request to the server-side endpoint instead of logging the selected region to the console.
+
+const regionSelect = document.getElementById("regionSelect");
+
+regionSelect.addEventListener("change", handleDropdownSelection);
+
+function handleDropdownSelection() {
+  const selectedRegion = regionSelect.value;
+
+  // Make an HTTP request to the server-side endpoint
+  fetch(`/countries?region=${selectedRegion}`)
+    .then((response) => response.json())
+    .then((data) => {
+      // Handle the response from the server
+      console.log("Filtered country list:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+// The client-side code handles the dropdown selection event and makes an HTTP request to the server-side endpoint using the fetch API. The response from the server is then logged to the console.
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //
 // Create a server-side file (e.g., server.js) that handles the server-side logic and exposes endpoints for client-side communication.
 // Move the server-side code from the combined code you provided into the server.js file. This code will handle the server-side functionality, such as reading the data file and sending the filtered country list.
+//In the server-side code, update the endpoint to receive the region from the request query string and send the filtered country list as the response.
 
 const express = require("express");
 const fs = require("fs");
@@ -28,3 +29,5 @@ app.get("/countries", function (req, res) {
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+// In this example, the server-side code is responsible for handling the /countries endpoint, which receives the region from the request query string and sends the filtered country list as the response.
